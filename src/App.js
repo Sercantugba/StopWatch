ae53895e80e981cdf1a385a9fe63c94df311c94a
Sercantugba <sercantugba834@gmail.com>
2020-07-29 18:07:00 -0500

    Creating app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <div className="Button">
+          <Button />
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
